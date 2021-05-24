54f827f0d8e8dcf4858d453b7838f7f6f77135de
CodyNeeraj <neerajkumar332424@gmail.com>
2021-05-24 15:21:16 +0530

    Changed L&F to Intellij native

--- src/main/StartFrame.java
+++ src/main/StartFrame.java
@@ -1,5 +1,6 @@
 package main;
 
+import com.formdev.flatlaf.FlatLightLaf;
 import function.Method;
 import java.io.IOException;
 import java.net.Socket;
@@ -24,6 +25,7 @@ public class StartFrame extends javax.swing.JFrame
         try
         {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            UIManager.setLookAndFeel(new FlatLightLaf());
             //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
             //will set the default installed l&F as windows Native
         }
